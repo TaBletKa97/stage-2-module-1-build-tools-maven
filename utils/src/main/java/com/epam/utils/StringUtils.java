cb107bdbdd7651773b7e594cfee7bc77ed1fc5ad
tabletka97 <mtabletka.by@gmail.com>
2023-02-14 15:08:01 +0300

    done with Apache Commons Lang and commit .iml file

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,9 @@
 package com.epam.utils;
 
+import static org.apache.commons.lang3.compare.ComparableUtils.is;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return Integer.parseInt(str) > 0;
+        return  is(str).greaterThan("0");
     }
 }
